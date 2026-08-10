4dcba31f34039db02eb7817d32ab26afb012f8df
RaffoAtt <55083017+RaffoAtt@users.noreply.github.com>
2026-08-10 17:41:27 +0200

    Delete all.js

--- all.js
+++
@@ -1,5 +0,0 @@
-// Placeholder - funzioni importate da app-old.js via refactor
-// Questo file sarà riempito gradualmente durante il refactor
-export function placeholder() {
-  console.log('Refactor in progress');
-}
